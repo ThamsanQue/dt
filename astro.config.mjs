b8a4d764c455f62f541c3bb815470a707f553032
thamsanqaj <thamsanqa.dev@gmail.com>
2024-04-29 16:13:03 +0200

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [react(), tailwind()],
 });
